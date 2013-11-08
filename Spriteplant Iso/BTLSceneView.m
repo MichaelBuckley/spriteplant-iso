470fa78d390f91e6e1242a94b81ecff7c3be22e3
Michael Buckley <thebuckley@gmail.com>
2013-11-08 07:31:09 -0800

    Experimenting with box drawing

--- Spriteplant Iso/BTLSceneView.m
+++ Spriteplant Iso/BTLSceneView.m
@@ -14,13 +14,49 @@
 {
     [self setScene: [SCNScene new]];
     
-    SCNLight *ambientLight = [SCNLight light];
-    SCNNode *ambientLightNode = [SCNNode node]; ambientLight.type = SCNLightTypeAmbient; ambientLight.color = [NSColor colorWithDeviceWhite:0.1
-                                                                                                                                      alpha:1.0];
-    [[self scene].rootNode addChildNode:ambientLightNode];
+    SCNLight *spotlight = [SCNLight light];
+    SCNNode *spotlightNode = [SCNNode node];
+    [spotlight setType: SCNLightTypeOmni];
+    [spotlight setColor: [NSColor colorWithDeviceWhite: 0.7 alpha:1.0]];
+    [spotlight setAttribute: [NSNumber numberWithInt:90] forKey: SCNLightSpotOuterAngleKey];
+    [spotlightNode setPosition: SCNVector3Make(0, 2, 0)];
+    //[spotlightNode setRotation: SCNVector4Make(1.0, 0, 0, -M_PI_2)];
     
-    SCNBox* box = [SCNBox boxWithWidth:1 height:1 length:1 chamferRadius:0.2];
+    [spotlightNode setLight: spotlight];
+    [[self scene].rootNode addChildNode: spotlightNode];
+    
+    SCNLight* ambientLight = [SCNLight light];
+    SCNNode* ambientLightNode = [SCNNode node];
+    
+    [ambientLight setType: SCNLightTypeAmbient];
+    [ambientLight setColor: [NSColor colorWithDeviceWhite: 0.01 alpha:1.0]];
+    [ambientLightNode setLight: ambientLight];
+    
+    [[self scene].rootNode addChildNode: ambientLightNode];
+    
+    SCNCamera *camera = [SCNCamera camera];
+    camera.xFov = 45;   // Degrees, not radians
+    camera.yFov = 45;
+    SCNNode *cameraNode = [SCNNode node];
+    cameraNode.camera = camera;
+    cameraNode.position = SCNVector3Make(0, 0, 2);
+    [[self scene].rootNode addChildNode:cameraNode];
+    
+    //SCNBox* box = [SCNCylinder cylinderWithRadius: 1.0 height:2.0];
+    //SCNBox* box = [SCNSphere sphereWithRadius: 1.0];
+    SCNBox* box = [SCNBox boxWithWidth:1 height:1 length:1 chamferRadius:0.0];
     SCNNode* boxNode = [SCNNode nodeWithGeometry: box];
+    [boxNode setRotation: SCNVector4Make(1.0, 0, 0, 0.942477796)];
+    
+    SCNMaterial* material = [SCNMaterial material];
+    [[material diffuse] setContents: [NSColor blueColor]];
+    [[material specular] setContents: [NSColor blueColor]];
+    //[[material emission] setContents: [NSColor redColor]];
+    [material setLocksAmbientWithDiffuse: YES];
+    [material setShininess: 0.0];
+    [material setLightingModelName: SCNLightingModelBlinn];
+    
+    [box setMaterials: @[ material ]];
     
     [[[self scene] rootNode] addChildNode: boxNode];
 }
